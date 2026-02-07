0c1972a1bfa4b096989cad059c5a8be672d15c13
mhzishans <mhzishans@eng.rizvi.edu.in>
2026-02-07 12:34:30 +0530

    Create EXP_1.py

---
+++ EXP_1.py
@@ -0,0 +1,7 @@
+name=input("enter your name: ")
+UIN=input("enter your UIN: ")
+marks=input("enter your marks: ")
+
+print("Hello!",name)
+print("My UIN is",UIN)
+print(f"I scored {marks} marks in 12th")
