e06826e6aefdfba9584ecabb0491dea4743db499
mhzishans <mhzishans@eng.rizvi.edu.in>
2026-02-07 12:50:15 +0530

    Create EXP_4.py

---
+++ EXP_4.py
@@ -0,0 +1,8 @@
+print("Date: 27/01/26, UIN: 251A015")
+
+Principalamount=float(input("enter the Principal Amount(in Rupees): "))
+Rateofinterest=float(input("enter the Rate Of Interest(In percent): "))
+
+Timeperiod=int(input("Enter the time period(In years): "))
+SimpleInterest=(Principalamount+Rateofinterest+Timeperiod)/100
+print("The Simple Interest is: ",SimpleInterest)
